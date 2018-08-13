47330e6e9bcadc0ba27db4910326abbd7582630e
kahyeelee@gmail.com <kahyeelee@gmail.com>
2018-08-13 11:26:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Add your functions and code here
-function destructivelyAppendKitten (kittens, )
+function destructivelyAppendKitten (array, element) {
+  kittens.push('Ralph');
+  return kittens;
+}
